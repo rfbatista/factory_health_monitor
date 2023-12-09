9e1d8983d7f851a28274a41f3570b7c1ba2be138
rfbatista <renfbatista@gmail.com>
2023-12-09 16:11:33 -0300

    feat: password value object

---
+++ backend/shared/password.ts
@@ -0,0 +1,33 @@
+import * as bcrypt from 'bcryptjs'
+
+export class Password {
+  private _salt: string;
+  private pwdhash: string;
+  private constructor(hash: string, salt: string) {
+    this.pwdhash = hash;
+    this._salt = salt
+  }
+
+  static async fromRaw(password: string): Promise<Password> {
+    const salt = await bcrypt.genSalt(10)
+    var hash = bcrypt.hashSync(password, salt);
+    return new Password(hash, salt);
+  }
+
+  static fromHash(pwdhash: string: salt: string) {
+    return new Password(pwdhash, salt);
+  }
+
+  get hash() {
+    return this.pwdhash;
+  }
+
+  get salt() {
+    return this._salt;
+  }
+
+  isEqualTo(value: string) {
+    var hash = bcrypt.hashSync(value, this._salt);
+    return this.hash === hash;
+  }
+}
